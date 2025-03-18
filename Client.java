9422df949b0ead0092f0186986d57905df723884
quincejuan1234 <127440848+quincejuan1234@users.noreply.github.com>
2025-03-18 16:15:16 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -47,16 +47,7 @@ public class Client {
                     break;
                 }
 
-                //query for how many times to run command(number)
-                System.out.println("Please Enter the Number of Requests");
-                System.out.println("Options: 1, 5, 10, 15, 20, 25");
-                int numThreads = in.nextInt();// get number of requests(threads) from the user
-
                 long totalTurnAroundTime = 0;
-
-                //make some threads
-                //run threads
-                //for(int i = 0; i < numThreads; i++) {
                 long startTime = System.nanoTime(); // Start time measurement
 
                 // Send command number to the Server
@@ -73,15 +64,9 @@ public class Client {
                 long turnAroundTime = endTime - startTime; // Elapsed time in nanoseconds
                 totalTurnAroundTime += turnAroundTime;
                 System.out.println("Turn around time: " + (turnAroundTime / 10000000) + " ms");// display time in milliseconds
-                    //System.out.println("Turn-around Time for request " + (i + 1) + ": " + turnAroundTime + " nanoseconds");
-                //}
-
+                
                 // Calculate and display the total turn-around times
                 System.out.println("Total turn around Time: " + (totalTurnAroundTime / 1000000) + " ms");// display time in milliseconds
-                //calculate average time
-                //System.out.println("Average Turn-around Time: " + ((totalTurnAroundTime / numThreads) / 1000000) + " nanoseconds");// display time in milliseconds
-
-
             }while(true);//end loop
         }//end try
         catch (UnknownHostException ex) {
@@ -91,6 +76,4 @@ public class Client {
             System.out.println("I/O error: " + ex.getMessage());
         }
     }//end main
-    /***/
-    //public ??? Thread_handler(){}
 }//end class
